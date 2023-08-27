f4fddf332d401534d41905c4de04429db6c4d2e0
Richer Duong <duongricher@gmail.com>
2023-08-27 15:37:20 -0500

    Complete HTML

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create basic calculation functions
 function add(a, b) {
   return a + b
 }
@@ -12,4 +13,28 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b
+}
+
+// Create variables for first number, operator, and second number
+let firstNumber = ''
+let operator = ''
+let secondNumber = ''
+
+// Create operate function that calls one of the basic math functions
+function operate(operator, a, b) {
+  a = Number(a)
+  b = Number(b)
+  switch (operator) {
+    case '+':
+      return add(a, b)
+    case '−':
+      return substract(a, b)
+    case '×':
+      return multiply(a, b)
+    case '÷':
+      if (b === 0) return null
+      else return divide(a, b)
+    default:
+      return null
+  }
 }
